c8c4b6cb401acab29c01f9735e2a00202bd66775
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-01-22 03:13:35 -0800

    Still working on stock_prices file, only some tests passing.

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -4,17 +4,31 @@ import argparse
 
 def find_max_profit(prices):
   current_min_index = 0
-  current_max_index = 0
+  current_max_index = 1
+  current_min = prices[current_min_index]
+  current_max = prices[current_max_index]
   for i in range(1, len(prices)):
     print(f"iteration: {i}")
-    if prices[i] < prices[current_min_index] and current_min_index <= i:
-      current_min_index = i
+    if prices[i] < prices[current_min_index] and current_min_index <= i and prices[current_max_index] > prices[current_min_index]:
+        print(f"  if {prices[i]} < {prices[current_min_index]} and {current_min_index} <= {i} and {prices[current_max_index]} >= {prices[current_min_index]}:")
+        current_min_index = i
+        print(f"    current_min_index is now {i}, the price is {prices[current_min_index]}")
     if prices[i] > prices[current_max_index] and current_min_index <= i:
+      print(f"if {prices[i]} > {prices[current_max_index]} and {current_min_index} <= {i}:")
       current_max_index = i
-    print(f"current_min_index is now {current_min_index}, the price is {prices[current_min_index]}")
-    print(f"current_max_index is now {current_max_index}, the price is {prices[current_max_index]}")      
+      print(f"    current_max_index is now {i}, the price is {prices[current_max_index]}")      
   print(f"min: {prices[current_min_index]}, max: {prices[current_max_index]}")
   return prices[current_max_index] - prices[current_min_index]
+
+# def find_max_profit(prices):
+#   current_min_index = 0
+#   current_max_index = 0
+#   current_min = prices[current_min_index]
+#   current_max = prices[current_max_index]
+
+#   for i in range(0, len(prices)):
+#     print(f"{prices[i]}")
+  
 
 
 if __name__ == '__main__':

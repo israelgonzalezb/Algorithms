fb14168e3d5919d69ac2e9fc11216bb76f4654df
Israel Gonzalez <48233351+israelgonzalezb@users.noreply.github.com>
2020-01-22 01:28:13 -0800

    Working on stock_prices function, tests not passing

--- stock_prices/stock_prices.py
+++ stock_prices/stock_prices.py
@@ -3,7 +3,18 @@
 import argparse
 
 def find_max_profit(prices):
-  pass
+  current_min_index = 0
+  current_max_index = 0
+  for i in range(1, len(prices)):
+    print(f"iteration: {i}")
+    if prices[i] < prices[current_min_index] and current_min_index <= i:
+      current_min_index = i
+    if prices[i] > prices[current_max_index] and current_min_index <= i:
+      current_max_index = i
+    print(f"current_min_index is now {current_min_index}, the price is {prices[current_min_index]}")
+    print(f"current_max_index is now {current_max_index}, the price is {prices[current_max_index]}")      
+  print(f"min: {prices[current_min_index]}, max: {prices[current_max_index]}")
+  return prices[current_max_index] - prices[current_min_index]
 
 
 if __name__ == '__main__':
